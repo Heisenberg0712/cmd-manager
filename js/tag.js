7179791938a9f5af8443e56cb9d32ba768594cb0
Kartik Prajapat <prajapatatwork0712@gmail.com>
2024-07-31 01:10:26 +0530

    added command list

--- js/tag.js
+++ js/tag.js
@@ -3,22 +3,23 @@ export function setupTagAdditionContainer() {
   const tagButtonContainer = document.getElementById("tagButtonContainer");
   const addTagInput = document.getElementById("addTagInput");
   const insertTagBtn = document.getElementById("insertTagBtn");
-
+  const colorPicker = document.getElementById("colorPicker");
   addTagInput.addEventListener("input", (e) => {
     newTagName = e.target.value;
   });
 
   insertTagBtn.addEventListener("click", () => {
     if (newTagName.length > 0) {
-      const buttonElement = createNewTag(newTagName);
+      const buttonElement = createNewTag(newTagName, colorPicker.value);
       tagButtonContainer.append(buttonElement);
     }
   });
 }
 
-function createNewTag(name) {
+function createNewTag(name, color) {
   const buttonElement = document.createElement("button");
   buttonElement.className = "bg-blue-500 hover:bg-blue-600 rounded w-fit px-2";
   buttonElement.innerHTML = name;
+  buttonElement.style.backgroundColor = color;
   return buttonElement;
 }
